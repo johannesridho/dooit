35be6aa76996ba5ad74b4631c5849e23b5a963d2
asyrafsalim <ninjatrickz@gmail.com>
2018-10-31 22:53:48 +0800

    add dart server api

---
+++ price_scraper/lib/price_scraper.dart
@@ -0,0 +1,11 @@
+/// price_scraper
+///
+/// A Aqueduct web server.
+library price_scraper;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
